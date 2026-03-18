8149f7196fc5de6f56f2b9ad801c5e34b0c4ebab
Sebastian Springer <Sebastian.Springer@rohde-schwarz.com>
2026-03-18 16:59:36 +0100

    [angular] 01: Lösung

--- angular-spa/src/app/book-card/book-card.ts
+++ angular-spa/src/app/book-card/book-card.ts
@@ -1,14 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { Book } from '../book.model';
 
 @Component({
   selector: 'app-book-card',
   templateUrl: './book-card.html',
 })
 export class BookCard {
-  // todo:
-  //   - Ein Input-Signal für das Buch definieren:
-  //     readonly book = input.required<Book>();
-  //   - Im Template die Eigenschaften von book() ausgeben
-  //     (Titel, Jahr, ISBN, Seiten)
-  //   - Die CSS-Klasse "Card" verwenden
+  readonly book = input.required<Book>();
 }
--- angular-spa/src/app/book-list/book-list.ts
+++ angular-spa/src/app/book-list/book-list.ts
@@ -1,18 +1,13 @@
 import { Component } from '@angular/core';
+import { BookCard } from '../book-card/book-card';
 import { Book } from '../book.model';
 import { getDemoBooks } from '../demo-data';
 
 @Component({
   selector: 'app-book-list',
+  imports: [BookCard],
   templateUrl: './book-list.html',
 })
 export class BookList {
-  // getDemoBooks liefert eine Liste mit Büchern
-  //  - im nächsten Schritt lesen wir die Liste vom Server
   readonly books: Book[] = getDemoBooks();
-
-  // todo:
-  //   - Im Template mit @for über die Bücher iterieren
-  //     und jeweils eine BookCard rendern
-  //   - Dabei 'track book.id' angeben
 }
